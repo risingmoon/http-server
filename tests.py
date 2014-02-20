d5bee5ad7e231e532b56fa9c04e0f80bd8ce0309
risingmoon <justindavidlee88@gmail.com>
2014-02-19 22:39:00 -0800

    Modified tests.py. Added test_parse_request

--- tests.py
+++ tests.py
@@ -5,18 +5,18 @@ from http_server import *
 MESSAGE = """GET /index.html HTTP/1.1\r\n"""
 METHODS= ["GET","POST","DELETE","UPDATE"]
 
-class http_serverTest(unittest.TestCase):
+class HttpServerTest(unittest.TestCase):
 
     def setUp(self):
          self.server_socket = socket_setup(ADDRESS, PORT)
          self.client_socket = socket_setup(ADDRESS, PORT)
 
-    def connect_client_server(self):
+    def connect_client_server(self,message):
         self.server_socket.bind((ADDRESS, PORT))
         self.server_socket.listen(1)
 
         self.client_socket.connect((ADDRESS, PORT))
-        self.client_socket.sendall(MESSAGE)
+        self.client_socket.sendall(message)
         self.client_socket.shutdown(socket.SHUT_WR)
 
         connection, address = self.server_socket.accept()
@@ -27,12 +27,12 @@ class http_serverTest(unittest.TestCase):
         return response
 
     def test_socket_setup(self):
-        response = self.connect_client_server()
+        response = self.connect_client_server(MESSAGE)
         self.assertEqual(response,MESSAGE)
 
-    def test_parse_request_method(self):
+    def test_parse_request(self):
         method, uri, protocol = parse_request(MESSAGE)
-        self.assertIn(method,METHODS)
+        self.assertIn(method, METHODS)
 
 if __name__ == '__main__':
     unittest.main()
